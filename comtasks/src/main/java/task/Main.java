82f082f1fe935b65c1775b9a5e3d9964f476d626
Artem_Pryzhkov <artem_pryzhkov@epam.com>
2017-12-20 12:45:36 +0200

    [FB-B] create method run

--- comtasks/src/main/java/task/Main.java
+++ comtasks/src/main/java/task/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First tasks");
         System.out.println("Second commit");
+        SomeRunner someRunner = new SomeRunner();
+        someRunner.run();
     }
 }
